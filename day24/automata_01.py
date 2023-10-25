cc015d68aff5f71af0689e693bcd27803c79c723
fareed23 <faroodcp9@gmail.com>
2023-10-25 23:31:16 +0500

    day24

---
+++ day24/automata_01.py
@@ -0,0 +1,9 @@
+# Automata
+# 1. Read input
+# 2. Create a grid of hexagons
+# 3. Create a hexagon class
+# 4. Create a hexagon grid class
+# 5. Create a function to parse the input
+# 6. Create a function to count the number of black tiles
+# 7. Create a function to flip the tiles
+# 8. Create a function to run the automata
