a2c5bdc48eebacbf7e464bcbe7666b595522ff13
fareed23 <faroodcp9@gmail.com>
2023-11-02 12:22:33 +0500

    day20

--- day20/streamlit.py
+++ day20/streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('My first STREAMLIT app')
+st.header('This Streamlit framework is used for Web App')
 st.sidebar("Sidebar of Streamlit app")
 st.markdown('This is markdown using Streamlit')
-st.header('This is header')
